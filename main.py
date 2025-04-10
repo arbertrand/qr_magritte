2f19b2af4d80c6ede5f6da9cb689616312121203
Antoine Bertrand <antoine.bertrand@gvl.de>
2025-04-10 10:11:57 +0200

    created

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import segno
 
 # Dein QR-Code-Inhalt
-data = 'https://example.com'
+data = 'https://raw.githubusercontent.com/arbertrand/qr_magritte/refs/heads/main/qr_magritte.png'
+# => tiny URL
+data = "https://tinyurl.com/4ycext9w"
 
 # Alle 8 Masken durchprobieren
 for mask in range(8):
     # QR-Code mit bestimmter Maske erzeugen
     qr = segno.make_qr(data, mask=mask)
+    # qr = segno.make_micro(data, mask=mask)
 
     # Datei speichern mit Masken-Nummer im Namen
     filename = f'result/qr_mask_{mask}.png'
-    qr.save(filename, scale=10)
+    qr.save(filename, scale=1)
     print(f'Gespeichert: {filename}')
