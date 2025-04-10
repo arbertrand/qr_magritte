123e819ab6878623a2cbbc67c7e9a91eb59a2891
Antoine Bertrand <antoine.bertrand@gvl.de>
2025-04-10 11:02:30 +0200

    beauty

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ import math
 bild = Image.open("./qr_magritte_raw.png").convert("RGBA")
 
 # Text, der hinzugefügt werden soll
-text = "Ceci est une ■▪ □ ▢ ▫"
+text = "Ce▢▢ n▪est pas un▢ ▪□" #21
+# Ceci n'est pas une pipe.
 
 # Font laden – du brauchst eine Schreibschrift-Datei, z.B. 'GreatVibes-Regular.ttf'
 # Kostenlos z.B. hier: https://fonts.google.com/specimen/Great+Vibes
@@ -43,11 +44,11 @@ draw = ImageDraw.Draw(text_layer)
 text_width, text_height = draw.textsize(text, font=font)
 
 # Position berechnen: unten links
-x = 30
-y = bild.width - text_width + 350
+x = 0
+y = bild.width - text_width + 450
 
 # Text schreiben
-draw.text((x, y), text, font=font, fill=(255, 69, 0, 255))  # rot
+draw.text((x, y), text, font=font, fill=(255, 69, 0, 200))  # rot
 
 # Layer rotieren (ca. 45° für von unten links nach oben rechts)
 text_layer = text_layer.rotate(45, resample=Image.BICUBIC, expand=1)
@@ -59,6 +60,26 @@ final_image.paste(bild, ((text_layer.width - bild.width) // 2, (text_layer.heigh
 # Textlayer drauflegen
 final_image = Image.alpha_composite(final_image, text_layer)
 
+
+# Ursprüngliche Bildgröße merken
+original_width, original_height = bild.size
+
+# Neue Bildgröße nach Rotation
+final_width, final_height = final_image.size
+
+# Berechne den Offset zum Zuschneiden (zentriert)
+left = (final_width - original_width) // 2
+top = (final_height - original_height) // 2
+right = left + original_width
+bottom = top + original_height
+
+# Zuschneiden
+final_image_cropped = final_image.crop((left, top, right, bottom))
+
 # Ergebnis speichern
-final_image.convert("RGB").save("qr_magritte.png", "PNG")
+final_image_cropped.convert("RGB").save("qr_magritte.png", "PNG")
+
+
+# Ergebnis speichern
+#final_image.convert("RGB").save("qr_magritte.png", "PNG")
 
